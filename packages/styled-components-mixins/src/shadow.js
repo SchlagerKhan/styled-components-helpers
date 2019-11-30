3d8bec5791fa78dcd98ec536192739148156bb5b
Kalle <kh.hansson@gmail.com>
2019-11-30 10:59:27 +0100

    Fixed shadows

--- packages/styled-components-mixins/src/shadow.js
+++ packages/styled-components-mixins/src/shadow.js
@@ -4,6 +4,6 @@ export const shadow = (x, y, blur, spread, color) => css`
 	box-shadow: ${x}px ${y}px ${blur}px ${spread}px ${color};
 `;
 
-export const dropShadow = shadow(0, 2, 4, 'rgba(0,0,0,0.2)');
+export const dropShadow = shadow(0, 2, 4, 0, 'rgba(0,0,0,0.2)');
 
-export const castShadow = shadow(0, -2, 4, 'rgba(0,0,0,0.2)');
+export const castShadow = shadow(0, -2, 4, 0, 'rgba(0,0,0,0.2)');
