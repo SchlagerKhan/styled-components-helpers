e8bd93f2baae6aeaefd3902f4148cc6138810360
Kalle <kh.hansson@gmail.com>
2019-12-12 07:26:50 +0100

    Added plainShadow

--- packages/styled-components-mixins/src/shadow.js
+++ packages/styled-components-mixins/src/shadow.js
@@ -5,6 +5,8 @@ export const shadow = (x, y, blur, spread, color) => css`
 	box-shadow: ${x}px ${y}px ${blur}px ${spread}px ${color};
 `;
 
+export const plainShadow = shadow(0, 0, 4, 0, 'rgba(0,0,0,0.2)');
+
 export const dropShadow = shadow(0, 2, 4, 0, 'rgba(0,0,0,0.2)');
 
 export const castShadow = shadow(0, -2, 4, 0, 'rgba(0,0,0,0.2)');
